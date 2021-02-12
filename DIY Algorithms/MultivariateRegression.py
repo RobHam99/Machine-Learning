9fc5a677e938063f692d35f06041e2ec04d3c034
RobHam <71784867+RobHam99@users.noreply.github.com>
2021-02-12 21:06:13 +0000

    Delete MultivariateRegression.py

--- DIY Algorithms/MultivariateRegression.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-dataset = pd.read_csv('winequality-white.csv')
-x = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
-print(y)
